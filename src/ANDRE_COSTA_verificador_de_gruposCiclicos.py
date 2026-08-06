3981dff77d52773190e233b54308fbaf1c69f363
André Araújo Sarmento Costa <andresarmentoc@icloud.com>
2026-08-06 15:52:40 -0300

    atualizações

--- src/ANDRE_COSTA_verificador_de_gruposCiclicos.py
+++ src/ANDRE_COSTA_verificador_de_gruposCiclicos.py
@@ -2,51 +2,59 @@
 Função que verifica se um elemento é gerador de um grupo.
 """
 
-def eh_gerador(grupo, elemento, ordem):
+def eh_gerador(tabela, elemento):
  
-    # o Grupo é o conjunto de elementos que serão inseridos na função
+    # a tabela é o conjunto de elementos que serão testados na função, será tratada no formato de matriz em python
     # o elemento é o que será testado se é capaz de gerar todos os outros elementos do grupo.
-    # a ordem é o número de elementos no grupo.
         
-
     a = 0  
     # a variavel "a" é um contador que vai contar quantos elementos distintos  o elemento escolhido gerou. 
 
     gerados = []  
     # Lista que vai armazenando os resultados (elemento^exp mod ordem)
 
-    for exp in range(1, ordem + 1):
-        # loop que percorre os expoentes de 1 até a ordem do grupo, para testar se o elemento escolhido gera os outros elementos do grupo.
+    atual = elemento
+    # 'atual' guarda o resultado da potência corrente do elemento.
+ 
+    for _ in range(len(tabela)):
+        # loop que percorre até o len(tabela) que é o tamanho do grupo, pois um gerador percorre todos elementos ate o fim do tamanho de seu grupo.
 
-        resultado = pow(elemento, exp, ordem)  
-        # Calcula elemento^exp e depois tira o módulo pela ordem do grupo.
+        if atual not in gerados:
+            # Se o elemento atual não estiver na lista, ele adicionará e somará +1 no contador "a".
 
-        if resultado not in gerados:
-            # Se o resultado não estiver na lista, ele adicionará e somará +1 no contador "a".
-
-            gerados.append(resultado)
+            gerados.append(atual)
             a += 1
+         
+        atual = tabela[atual][elemento]
+        #É feito um cruzamento na tabela para descobrir o resultado do numero "atual" com "elemento", equivalente á atual= atual * elemento
 
-    if a == len(grupo):
+             
+    if a == len(tabela):
         return True
     return False
     # Se o valor de "a" for igual ao tamanho do grupo, significa que o elemento testado é gerador, pois gerou todos os elementos do grupo, retornando true.
     #  Caso contrário, ele não é gerador retornando false.
 
-
 """
-Função que verifica se o grupo é cíclico, ou seja, se existe pelo menos
+Função que verifica se o grupo da tabela é cíclico, ou seja, se existe pelo menos
 um elemento capaz de gerar todos os outros elementos do grupo.
 """
-def eh_ciclico(grupo, ordem):
-    #O grupo a ser testado é o mesmo que foi testado na função eh_gerador, e a ordem é o número de elementos no grupo.
+def eh_ciclico(tabela, identidade):
+    #A tabela a ser testada é a mesma que foi testada na função eh_gerador.
 
-    for num in grupo:
-        # loop que percorre cada elemento do grupo, testando um por um como possível gerador, testando se cada elemento é gerador apartir da função eh_gerador.
-        # Caso exista algum elemento que seja gerador, a função retornará True e o próprio elemento gerador encontrado.
-        # Caso contrário, retornará False e None, pois não há elemento que seja gerador.
+    n = len(tabela)
+    # Tamanho do grupo = número de elementos = número de linhas da tabela.
 
-        if eh_gerador(grupo, num, ordem):
+    for num in range(n):
+       # loop que percorre cada elemento do grupo de 0 ate n-1
+           # Serão testados um  elemento por um como possível gerador, testando se cada elemento é gerador apartir da função eh_gerador.
+           # Caso exista algum elemento que seja gerador, a função retornará True e o próprio elemento gerador encontrado.
+           # Caso contrário, retornará False e None, pois não há elemento que seja gerador.
+
+        if eh_gerador(tabela, num, identidade):
+        # Se nenhum elemento gerar o grupo todo, ele não é cíclico.  
+
             return True, num
-
     return False, None
+
+
